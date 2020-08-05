3eca674e15b2b6a2d552007116660d4f41657a75
armando-fandango <armando@neurasights.com@users.noreply.github.com>
2020-08-05 00:44:12 -0400

    bug fixed

--- docker/ez-conda-create.sh
+++ docker/ez-conda-create.sh
@@ -32,7 +32,7 @@ conda activate $venv || \
     #jupyter nbextension enable ipyparallel && \
 
 conda activate $venv && \
-    conda install -y -p $venv $opts --file $(dirname "$(readlink -f -- '${BASH_SOURCE}/requirements-conda.txt')") && \
-    pip install --no-deps --use-feature 2020-resolver -r $(dirname "$(readlink -f -- '${0}/requirements-pip.txt')")
+    conda install -y -p $venv $opts --file ./requirements-conda.txt')") && \
+    pip install --no-deps --use-feature 2020-resolver -r ./requirements-pip.txt')")
 
 
